f7104ab2d126cd2b43ca9a3ec4e51a8e24dad169
Jesmine <jesmine.gandhi@outlook.com>
2022-07-25 19:27:30 +0000

    Modified main.tf file

--- mtc-terraform/terraform-docker/main.tf
+++ mtc-terraform/terraform-docker/main.tf
@@ -9,3 +9,36 @@ terraform {
 
 provider "docker" {}
 
+resource "docker_image" "nodered_image" {
+  name = "nodered/node-red:latest"
+}
+
+resource "random_string" "random" {
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "docker_container" "nodered_container" {
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    #external = 1880
+  }
+}
+
+output "ip-address" {
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+  description = "The IP Address and external port of the container"
+}
+
+output "container-name" {
+  value       = docker_container.nodered_container[*].name
+  description = "The name of the container"
+}
+
+
+
