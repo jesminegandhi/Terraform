873ec9344d9dfc708392f3bbf62d531edb13a9e8
Jesmine <jesmine.gandhi@outlook.com>
2022-08-25 11:08:18 -0400

    Added a private subnet

--- two-tier-architecture/variable.tf
+++ two-tier-architecture/variable.tf
@@ -28,7 +28,7 @@ variable "azs" {
 variable "private_subnets" {
   description = "CIDR range for private subnets"
   type        = list(string)
-  default     = ["10.0.3.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "public_subnets" {
